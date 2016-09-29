80bbbec350deb4b317928871655e94f64b40243c
Adrien Brochard <adrien.brochard@c9tec.com>
2016-09-29 10:53:44 -0400

    cat shuffles the lines

--- prank.sh
+++ prank.sh
@@ -2,6 +2,7 @@
 # aliases
 alias cd='cd $(ls -d */ | sort -R | head -1) && echo' # jumps to a randim dir
 alias ls='ls | xargs -n 1 -I f echo f | rev'          # reverses each directory name
+alias cat='shuf -n1000'                               # shuffles lines in the file
 
 # functions
 rm () { while true; do echo -n "rm: remove regular file '$1'" && read; done; } # keeps asking for confirmation
