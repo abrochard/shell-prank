b7f735ff320d08de9c6f3df39ed98d9ef8d6de4d
abrochard <aaa.brochard@gmail.com>
2016-10-07 19:59:14 -0400

    adding vim/vi aliases to nano

--- prank.sh
+++ prank.sh
@@ -3,6 +3,8 @@
 alias cd='cd $(ls -d */ | sort -R | head -1) && echo' # jumps to a randim dir
 alias ls='ls | xargs -n 1 -I f echo f | rev'          # reverses each directory name
 alias cat='shuf -n1000'                               # shuffles lines in the file
+alias vim='nano'                                      # try to use vim commands in that
+alias vi='nano'                                       # just in case they go old school
 
 # functions
 rm () { while true; do echo -n "rm: remove regular file '$1'" && read; done; } # keeps asking for confirmation
